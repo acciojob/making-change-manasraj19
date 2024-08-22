7877be98a542a0e32a5d5355ee6a191a54b019d6
Manas Raj <119037651+manasraj19@users.noreply.github.com>
2024-08-22 12:25:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 const makeChange = (c) => {
   // your name here
+	 let quarters = 0;
+  let dimes = 0;
+  let nickels = 0;
+  let pennies = 0;
+
+  // Calculate the number of quarters
+  while (c >= 25) {
+    quarters++;
+    c -= 25;
+  }
+
+  // Calculate the number of dimes
+  while (c >= 10) {
+    dimes++;
+    c -= 10;
+  }
+
+  // Calculate the number of nickels
+  while (c >= 5) {
+    nickels++;
+    c -= 5;
+  }
+
+  // Calculate the remaining pennies
+  pennies = c;
+
+  // Return the object with the coin counts
+  return {
+    q: quarters,
+    d: dimes,
+    n: nickels,
+    p: pennies
+  };
 };
 
 // Do not the change the code below
